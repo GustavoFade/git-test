b6bbba9515fef7208a06138b4a1e299ffc40f4d4
fadel <gustavo-959694@hotmail.com>
2021-10-06 21:48:33 -0300

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,5 @@
+import './styles/global.scss';
+
+export function App(){
+    return <h1>Hi my friend !</h1>
+}
---
+++ src/index.jsx
@@ -0,0 +1,4 @@
+const { render } = require("react-dom");
+const { App } = require("./app");
+
+render(<App />, document.getElementById('root'))
